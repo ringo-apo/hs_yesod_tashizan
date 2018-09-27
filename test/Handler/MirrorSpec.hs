8638f5e6615ae7d1a24003b7d6cf5fe1d55f0068
Akira Taira <ringo1625@gmail.com>
2018-09-28 07:39:31 +0900

    201809280742

---
+++ test/Handler/MirrorSpec.hs
@@ -0,0 +1,14 @@
+module Handler.MirrorSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getMirrorR" $ do
+        error "Spec not implemented: getMirrorR"
+
+
+    describe "postMirrorR" $ do
+        error "Spec not implemented: postMirrorR"
+
